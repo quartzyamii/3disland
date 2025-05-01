05caa357b4f40a66f8b97f49cad854f8c374ea49
김민진 <kimminjin@gimminjin-ui-MacBookPro.local>
2025-05-01 21:42:16 +0900

    COMMIT MESSAGE

--- src/components/Popup.jsx
+++ src/components/Popup.jsx
@@ -11,7 +11,7 @@ const Popup = ({ onClose }) => {
   return (
     <animated.div 
       style={fadeIn}
-      className="fixed right-0 top-2/3 transform -translate-y-1/2 flex items-center justify-center z-50 mr-4"
+      className="fixed right-0 top-2/3 left-1/2 transform -translate-y-1/2 -x-1/5  flex items-center justify-center z-50 mr-4"
     >
       <div className="bg-white/70 backdrop-blur-sm p-6 rounded-lg shadow-xl w-[600px]">
         <div className="flex gap-6">
@@ -32,7 +32,11 @@ const Popup = ({ onClose }) => {
             <div>
               <h2 className="text-2xl font-bold mb-4">파이브-미닛키즈 야광팔찌</h2>
               <div className="w-full h-px bg-gray-200 mb-4"></div>
-              <p className="text-gray-600">2024년 10월의 야광팔찌</p>
+              <p className="text-gray-600">
+                2024년 10월 어느날의 야광팔찌<br />
+                학교에서 '예술제'와 함께 '파이브-미닛 키즈'퍼레이드 공연이 열렸다. 
+                그 공연에서 야광팔찌를 착용하고 퍼레이드를 구경하던 경험은 정말 즐거웠다. 
+              </p>
             </div>
             <div className="flex justify-end">
               <button
